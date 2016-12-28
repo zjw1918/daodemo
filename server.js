2ad795be3ef5a1f9395105454d4beffc2164a0d7
zjw1918 <zjw1918@sina.cn>
2016-12-28 15:41:03 +0800

    change hello

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   })
-  res.end('Hello World!')
+  res.end('Hello World xxx!')
 })
 
 server.listen(process.env.PORT || 80, function() {
