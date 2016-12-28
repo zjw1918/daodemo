0a123a1934b8b6ba7c9c1d16c2a0af0403d0281c
zjw1918 <zjw1918@sina.cn>
2016-12-28 17:29:29 +0800

    change 2

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   })
-  res.end('Hello World xxx111!')
+  res.end('Hello World 333!')
 })
 
 server.listen(process.env.PORT || 80, function() {
+  console.log(111);
   console.log('Docker DEMO with Node.js is running.')
 })
