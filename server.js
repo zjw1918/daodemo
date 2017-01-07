8952a3a76d36d0952d41412781ac7c234f77bccc
zjw1918 <zjw1918@sina.cn>
2017-01-07 18:16:40 +0800

    use heroku
    user heroku, install express

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-var http = require('http')
+// var http = require('http')
+//
+// var server = http.createServer(function(req, res) {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   })
+//   res.end('Hello World 4!')
+// })
+//
+// server.listen(process.env.PORT || 80, function() {
+//   console.log('Docker DEMO with Node.js is running.')
+// })
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.end('Hello World 4!')
-})
+//
+var express = require('express');
+var app = express();
 
-server.listen(process.env.PORT || 80, function() {
-  console.log('Docker DEMO with Node.js is running.')
-})
+app.get('/', function (req, res) {
+  res.send('Hello World -express!');
+});
+
+var server = app.listen(process.env.PORT || 80, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
