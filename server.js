8f661e23f507f7f43202e8c7c03896b6c73801e1
zjw1918 <zjw1918@sina.cn>
2017-01-07 18:39:53 +0800

    heroku2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World -express!');
+  res.send('Hello World -express2!');
 });
 
 var server = app.listen(process.env.PORT || 80, function () {
